d1c068ff04c4b96461d6c46d0236aed3cbf0a091
BedirhanGurler <bgurler001@gmail.com>
2025-09-27 00:44:59 +0300

    **console update

--- SecilStore.TestConsoleApp/Program.cs
+++ SecilStore.TestConsoleApp/Program.cs
@@ -1,10 +1,34 @@
 ﻿using SecilStore.ConfigLib;
 
-Console.WriteLine("Merhaba, Bu Seçil Store Case Çalışmasının Test Aşamasıdır");
-Console.WriteLine("---------------------------------------------------------");
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var reader = new ConfigurationReader(
+            connectionString: "Data Source=.;Initial Catalog=SecilStoreConfigLib;Integrated Security=True",
+            applicationName: "SERVICE-A",
+            intervalInMs: 5000
+        );
 
+        Console.WriteLine("Config Reader başlatıldı! 5 saniyede bir DB kontrolü yapılacak değişiklik yansıtılacak.");
+        Console.WriteLine("-------------------------");
 
-var reader = new ConfigurationReader(connectionString: "Data Source=.;Initial Catalog=SecilStoreConfigLib;Integrated Security=True", applicationName: "SecilStore.TestConsoleApp", intervalInMs: 5000);
-string site = reader.GetValue<string>("SiteName");
+        while (true)
+        {
+            try
+            {
+                string siteName = reader.GetValue<string>("SiteName");
+                bool basketEnabled = reader.GetValue<bool>("IsBasketEnabled");
+                int maxItemCount = reader.GetValue<int>("MaxItemCount");
 
-Console.WriteLine($"Site Name: {site}");
+                Console.WriteLine($"{DateTime.Now:T} - SiteName: {siteName}, BasketEnabled: {basketEnabled}, MaxItemCount: {maxItemCount}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata: {ex.Message}");
+            }
+
+            await Task.Delay(5000);
+        }
+    }
+}
